4af5b84829a8b6707adc470172a0b5d9b8e469bb
mariwp <142070393+mariwp@users.noreply.github.com>
2024-07-01 10:45:41 -0400

    Updated Ninja class

--- ninja.js
+++ ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
     constructor(name) {
         this.name = name;
-        this.health = 100; // Asignando un valor predeterminado de salud
-        this.speed = 3; // Asignando un valor predeterminado de velocidad
-        this.strength = 3; // Asignando un valor predeterminado de fuerza
+        this.health = 100; 
+        this.speed = 3; 
+        this.strength = 3; 
     }
 
     sayName() {
@@ -21,7 +21,6 @@ class Ninja {
 
 // Ejemplo de uso:
 const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName(); // "Hyabusa"
-ninja1.showStats(); // "Name: Hyabusa, Strength: 3, Speed: 3, Health: 100"
-ninja1.drinkSake();
-ninja1.showStats(); // "Name: Hyabusa, Strength: 3, Speed: 3, Health: 110"
+ninja1.sayName();
+ninja1.showStats();
+
