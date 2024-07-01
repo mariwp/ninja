dddc5c31ccb32debeb0d57a80f718aa95df1b4c2
mariwp <142070393+mariwp@users.noreply.github.com>
2024-07-01 11:01:19 -0400

    Added Sensei class with its own methods

--- ninja.js
+++ ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
     constructor(name) {
         this.name = name;
-        this.health = 100; 
-        this.speed = 3; 
-        this.strength = 3; 
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
     }
 
     sayName() {
@@ -19,8 +19,29 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        this.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+
 // Ejemplo de uso:
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
 
